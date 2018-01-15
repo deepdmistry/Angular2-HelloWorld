ade927ce3a79d87967b3096358d1555752929705
deepmistry <deepdmistry@gmail.com>
2018-01-15 12:04:47 -0500

    Reverted all backend related changes.
    Switched to using a fully blown app. ( Direct download and replace of all work )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,38 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 
-import {AppComponent} from './app.component';
-import {DashboardComponent} from './dashboard.component';
-import {HeroDetailComponent} from './hero-detail.component';
-import {HeroesComponent} from './heroes.component';
-import {HeroService} from './hero.service';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
 
 import {AppRoutingModule} from './app-routing.module';
-// Imports for loading & configuring the in-memory web api
-import {HeroSearchComponent} from "./hero-search.component";
+
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+import {HeroesComponent} from './heroes/heroes.component';
+import {HeroSearchComponent} from './hero-search/hero-search.component';
+import {HeroService} from './hero.service';
+import {MessageService} from './message.service';
+import {MessagesComponent} from './messages/messages.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    AppRoutingModule
+    FormsModule, AppRoutingModule, HttpClientModule,
+  
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false})
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
-    HeroDetailComponent,
-    HeroesComponent,
+    HeroesComponent, HeroDetailComponent, MessagesComponent,
     HeroSearchComponent
-  ],
-  providers: [HeroService],
-  bootstrap: [AppComponent]
+  ], providers: [HeroService, MessageService], bootstrap: [AppComponent]
 })
 export class AppModule {
 }
